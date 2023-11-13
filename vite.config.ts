3d5229ec5aa317c928fd96070565883683d4c604
niyonggui <niyg@mw-robot.com>
2023-11-13 09:49:19 +0800

    feat: 修改vitep配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/mui-pad-hmi/",
   plugins: [react()],
+  optimizeDeps: {
+    include: ["react/jsx-runtime"],
+  },
 });
